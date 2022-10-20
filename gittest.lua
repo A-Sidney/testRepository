fd169c03baa8229f9b47d77dc097c6fa108b32ec
A.Sidney <adorasidney@gmail.com>
2022-10-20 18:17:12 +0200

    Initial commit :wq

---
+++ gittest.lua
@@ -0,0 +1,15 @@
+local arr = {}
+
+local counter = 0
+
+local function count(val)
+    while val < 10 do
+        table.insert(arr, 1, val)
+        print("Value at ".. val)
+        val = val + 1
+    end
+end
+count(counter)
+print("Counter: ".. counter)
+print("Array: "..table.concat(arr, ", "))
+print("I changed something")
